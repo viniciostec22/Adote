fd55204beb1a8e113c62c389c387dfbc1e9bf40d
VINICIOS MATHEUS OLIVEIRA DA SILVA <vinicios.tec22@gmail.com>
2023-01-09 16:51:11 -0300

    v1.0

---
+++ accounts/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+def cadastro(request):
+    if request.user.is_authenticated:
+        return redirect('/divulgar/novo_pet')
+    if request.method == 'GET':
+        return render(request, 'accounts/cadastro.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return render(request, 'accounts/cadastro.html')
+        if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Digite as senhas iguais')
+            return render(request, 'accounts/cadastro.html')
+        
+        try:
+            user = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            messages.add_message(request, constants.SUCCESS, 'Usuário criado com sucesso')
+            return render(request, 'accounts/cadastro.html')
+        except: 
+            messages.add_message(request, constants.ERROR, 'Erro Interno do sistema tente novamente mais tarde')
+            return render(request, 'accounts/cadastro.html')
+
+def logar(request):
+    if request.user.is_authenticated:
+        return redirect('/divulgar/novo_pet')
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+        user = authenticate(username=nome, password=senha)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('/divulgar/novo_pet')
+        else:
+            messages.add_message(request, constants.ERROR, 'Usuario ou senha incorreto')
+            return render(request, 'accounts/login.html')
+        
+def sair(request):
+    logout(request)
+    return redirect('/auth/login')
